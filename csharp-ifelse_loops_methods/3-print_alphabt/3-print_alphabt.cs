34916511142f308e5bcf199df32100a1b074ed8a
Caseycjc <knightlaggia@gmail.com>
2023-05-11 02:15:58 -0500

    task3

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char currentChar = 'a';
+
+        do
+        {
+            if (currentChar != 'q' && currentChar != 'e')
+            {
+                Console.Write(currentChar);
+            }
+
+            currentChar++;
+        } while (currentChar <= 'z');
+    }
+}
