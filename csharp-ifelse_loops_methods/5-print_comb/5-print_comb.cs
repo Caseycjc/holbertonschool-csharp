1fa357dec3fb716a4ebc6cec4c5b0d5539ec9bc7
Caseycjc <knightlaggia@gmail.com>
2023-05-11 02:41:10 -0500

    pls

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -4,9 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        string[] numbers = new string[100];
+        
         for (int number = 0; number < 100; number++)
         {
-            Console.Write(number.ToString("00") + (number < 99 ? ", " : ""));
+            numbers[number] = number.ToString("00");
         }
+
+        Console.Write(string.Join(", ", numbers));
     }
 }
