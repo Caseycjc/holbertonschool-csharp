b0f983e62b965fbc9684d487494d637babc8736d
Caseycjc <knightlaggia@gmail.com>
2023-05-11 02:32:42 -0500

    task5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int number = 0; number < 100; number++)
+        {
+            Console.Write(number.ToString("00"));
+            
+            if (number < 99)
+            {
+                Console.Write(", ");
+            }
+        }
+    }
+}
