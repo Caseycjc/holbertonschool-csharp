71fa13ea798f3357c8b2e9e76eaaacef3ad132b6
Caseycjc <knightlaggia@gmail.com>
2023-05-11 11:24:03 -0500

    task6

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -4,19 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        int count = 0;
-
-        for (int i = 0; i <= 8; i++)
+        for (int i = 0; i <= 9; i++)
         {
-            for (int j = i + 1; j <= 9; j++)
+            for (int j = 0; j <= 9; j++)
             {
-                count++;
-                Console.Write($"{i}{j}");
-
-                // Check if it's not the last combination
-                if (count < 36)
+                if (i < j)
                 {
-                    Console.Write(", ");
+                    Console.Write($"{i}{j}, ");
                 }
             }
         }
