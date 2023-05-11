4852854587f58212b06f0939e9731ce47c8be6d2
Caseycjc <knightlaggia@gmail.com>
2023-05-11 09:46:49 -0500

    task6

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -6,7 +6,7 @@ class Program
     {
         int count = 0;
 
-        for (int i = 0; i <= 9; i++)
+        for (int i = 0; i <= 8; i++)
         {
             for (int j = i + 1; j <= 9; j++)
             {
