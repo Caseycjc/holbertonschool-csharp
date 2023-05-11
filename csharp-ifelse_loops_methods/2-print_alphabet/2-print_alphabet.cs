593bdb22bf5428a7f89e617964e7ea08a7b2b9ab
Caseycjc <knightlaggia@gmail.com>
2023-05-11 02:11:00 -0500

    task2

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char currentChar = 'a';
+        
+        do
+        {
+            Console.Write(currentChar);
+            currentChar++;
+        } while (currentChar <= 'z');
+    }
+}
