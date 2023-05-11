66fb7d6e9327fae88ea71e63c66d4cdbe4c571ff
Caseycjc <knightlaggia@gmail.com>
2023-05-11 02:49:46 -0500

    task6

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 9; i++)
+        {
+            for (int j = i + 1; j <= 9; j++)
+            {
+                Console.Write($"{i}{j}, ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
