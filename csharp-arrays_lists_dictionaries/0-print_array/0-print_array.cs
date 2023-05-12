ee946b5faf34574709815ecf1179b4fe707676c2
Caseycjc <knightlaggia@gmail.com>
2023-05-12 00:46:27 -0500

    task0

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,27 @@
+﻿public class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return new int[0];
+        }
+
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = i;
+            Console.Write(array[i] + " ");
+        }
+
+        Console.WriteLine();
+        return array;
+    }
+}
