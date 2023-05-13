cdecd5821c6fc4b8f57c7b69108a71b23fa686f1
Caseycjc <knightlaggia@gmail.com>
2023-05-12 23:14:13 -0500

    task0

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> uniqueNumbers = new HashSet<int>(myList);
+        int sum = 0;
+        foreach (int num in uniqueNumbers)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+}
