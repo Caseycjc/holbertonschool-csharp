6918282c53a6c698adf6886b3b452f6f6768bef2
Caseycjc <knightlaggia@gmail.com>
2023-05-12 23:25:09 -0500

    task2

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> set1 = new HashSet<int>(list1);
+        HashSet<int> set2 = new HashSet<int>(list2);
+
+        set1.SymmetricExceptWith(set2);
+
+        List<int> result = new List<int>(set1);
+
+        for (int i = 0; i < result.Count - 1; i++)
+        {
+            for (int j = i + 1; j < result.Count; j++)
+            {
+                if (result[j] < result[i])
+                {
+                    int temp = result[i];
+                    result[i] = result[j];
+                    result[j] = temp;
+                }
+            }
+        }
+
+        return result;
+    }
+}
