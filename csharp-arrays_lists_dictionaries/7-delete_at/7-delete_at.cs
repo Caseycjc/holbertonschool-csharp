ae6a81b2bf59afcda2248cc232e4bbfd2aff3fa6
Caseycjc <knightlaggia@gmail.com>
2023-05-12 21:42:20 -0500

    task7

--- csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -3,19 +3,14 @@ using System.Collections.Generic;
 
 public class List
 {
-    public static void DeleteAt(List<int> myList, int index)
+    public static List<int> DeleteAt( List<int> myList, int index)
     {
-        if (index < 0 || index >= myList.Count)
+        if(index < 0 || index >= myList.Count)
         {
             Console.WriteLine("Index is out of range");
-            return;
+            return myList;
         }
-
-        for (int i = index; i < myList.Count - 1; i++)
-        {
-            myList[i] = myList[i + 1];
-        }
-
-        myList.RemoveAt(myList.Count - 1);
+        myList.Remove(myList[index]);
+        return myList;
     }
 }
