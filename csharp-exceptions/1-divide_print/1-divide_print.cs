8484b268df131155acd25ba07a74b0002ac71a39
Caseycjc <knightlaggia@gmail.com>
2023-05-17 11:02:31 -0500

    task1

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Int
+{
+    public static void divide(int a, int b)
+    {
+        try
+        {
+        int a = 10;
+        int b = 0;
+        int result = a / b;
+        Console.WriteLine(result);
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Error: Division by zero");
+        }
+        finally
+        {
+        Console.WriteLine("Cleanup tasks are performed here.");
+        }
+    }
+    
+}
