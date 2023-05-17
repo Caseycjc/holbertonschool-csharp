2ebe895973278bc9693315595babe2c52ba54d5c
Caseycjc <knightlaggia@gmail.com>
2023-05-17 10:25:02 -0500

    Directory, Read me, and task 1

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ListHelper
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+        try
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (i < myList.Count)
+                {
+                    Console.WriteLine(myList[i]);
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+        return count;
+    }
+}
---
+++ csharp-exceptions/promac.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+dotnet new console -o "0-safe_list_print"
+dotnet build "0-safe_list_print"
