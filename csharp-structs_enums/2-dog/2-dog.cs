b9b0f4a6b1e37f21c635828b93df6fea25929f89
Caseycjc <knightlaggia@gmail.com>
2023-05-17 21:43:17 -0500

    task2

--- csharp-structs_enums/2-dog/2-dog.cs
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -1,15 +1,25 @@
-﻿public struct Dog
+﻿using System;
+
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
 {
     public string name;
     public float age;
     public string owner;
     public Rating rating;
 
-    public Dog(string name, float age, string owner, Rating rating)
+    public Dog(string name, float age, string owner, Rating rate)
     {
         this.name = name;
         this.age = age;
         this.owner = owner;
-        this.rating = rating;
+        this.rating = rate;
     }
 }
