38d0df7514892672b388d9823a02e5c7bc0e9ea7
Caseycjc <knightlaggia@gmail.com>
2023-05-24 19:21:25 -0500

    task2

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    /// This is a class
+    /// </summary>
+    public class Zombie
+    {
+        ///<summary>
+        /// public int
+        ///</summary>
+        public int health;
+        ///<summary>
+        /// public constructor
+        ///</summary>
+
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        ///<summary>
+        /// new constructor that is for the zombie's health
+        ///</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+    }
+}
