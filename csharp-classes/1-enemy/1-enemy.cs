99ae66852b274b5c0251b5187ba0f37d4670c470
Caseycjc <knightlaggia@gmail.com>
2023-05-24 19:07:09 -0500

    task1

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    /// This is a class
+    /// </summary>
+    public class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
