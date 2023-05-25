246617473bde1b4c85b82590b1339a68016fa3b4
Caseycjc <knightlaggia@gmail.com>
2023-05-24 22:54:18 -0500

    task1

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// This class contains methods to check the type of objects.
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Determines whether the provided object is an int.
+    /// </summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+
+    /// <summary>
+    /// Determines whether the provided object is an instance of, or an instance of a class that inherited from, Array.
+    /// </summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
