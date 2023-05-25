1d2004f852eb5a32de428cc16e4bab565e0be7ed
Caseycjc <knightlaggia@gmail.com>
2023-05-24 19:52:01 -0500

    task4

---
+++ csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    /// Defines a Zombie.
+    ///</summary>
+    public class Zombie
+    {
+        ///<summary>
+        /// Private field for the health of the Zombie.
+        ///</summary>
+        private int health;
+
+        ///<summary>
+        /// Private field for the name of the Zombie. Default is "(No name)".
+        ///</summary>
+        private string name = "(No name)";
+
+        ///<summary>
+        /// Public constructor that sets health to 0.
+        ///</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        ///<summary>
+        /// Throws ArgumentException if the value is less than 0.
+        ///</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+
+        ///<summary>
+        /// Public property for name. Allows getting and setting the name.
+        ///</summary>
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        ///<summary>
+        /// Returns the health of the Zombie.
+        ///</summary>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+    }
+}
