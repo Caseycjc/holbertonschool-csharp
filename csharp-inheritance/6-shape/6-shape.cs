6f6711aa8aa89488886fe262e8461780178d1911
Caseycjc <knightlaggia@gmail.com>
2023-05-25 22:49:00 -0500

    task6

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -6,10 +6,8 @@
 public class Shape
 {
     /// <summary>
-    /// Computes the area of the shape.
+    /// computes the area of the shape
     /// </summary>
-    /// <returns>The area of the shape.</returns>
-    /// <exception cref="NotImplementedException">Thrown when the method is not implemented.</exception>
     public virtual int Area()
     {
         throw new NotImplementedException("Area() is not implemented");
