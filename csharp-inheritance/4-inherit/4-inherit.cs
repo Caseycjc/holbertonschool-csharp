b9b24f2164f83aff9a1eda203691bb84787853d6
Caseycjc <knightlaggia@gmail.com>
2023-05-25 22:31:01 -0500

    task4

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// animal class
+/// </summary>
+public class Animal
+{
+
+}
+
+/// <summary>
+/// dog class that inherits from animal
+/// </summary>
+public class Dog : Animal
+{
+
+}
