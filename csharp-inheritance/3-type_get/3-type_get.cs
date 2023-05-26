aea9df7f151ae451a4c9688fc694097186af8d72
Caseycjc <knightlaggia@gmail.com>
2023-05-25 22:20:55 -0500

    task3

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+/// <summary>
+/// This class contains methods to check the type of objects.
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Prints the names of the available properties and methods of an object.
+    /// </summary>
+    /// <param name="myObj">The object to inspect.</param>
+    public static void Print(object myObj)
+    {
+        if (myObj == null)
+        {
+            Console.WriteLine("Object is null");
+            return;
+        }
+
+        Type type = myObj.GetType();
+
+        Console.WriteLine("Properties:");
+        foreach (PropertyInfo property in type.GetProperties())
+        {
+            Console.WriteLine(property.Name);
+        }
+
+        Console.WriteLine("Methods:");
+        foreach (MethodInfo method in type.GetMethods())
+        {
+            // Exclude system defined methods
+            if (method.DeclaringType == typeof(object))
+            {
+                continue;
+            }
+
+            Console.WriteLine(method.Name);
+        }
+    }
+}
