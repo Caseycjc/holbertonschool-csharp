e1048e2ee693d4c0884acc91c77ec1aca76af06b
Caseycjc <knightlaggia@gmail.com>
2023-05-31 18:06:40 -0500

    task3

--- csharp-generics/3-peek/queue.cs
+++ csharp-generics/3-peek/queue.cs
@@ -80,7 +80,7 @@ public class Queue<T>
     {
         if (Head == null)
         {
-            Console.WriteLine("Queue is empty.");
+            Console.WriteLine("Queue is empty");
             return default(T);
         }
 
