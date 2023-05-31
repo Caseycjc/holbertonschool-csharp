fdb80a1f452936f256c09ea0f11ba35581cc718a
Caseycjc <knightlaggia@gmail.com>
2023-05-31 16:45:37 -0500

    task0

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary> queue class </summary>
+public class Queue<T>
+{
+
+    /// <summary> returns the queues type </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+}
