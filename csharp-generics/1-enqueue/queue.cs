ff5cfa938b1122ab587817ceedb3d85497357306
Caseycjc <knightlaggia@gmail.com>
2023-05-31 16:57:21 -0500

    task1

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+/// <summary>
+/// Queue class.
+/// </summary>
+public class Queue<T>
+{
+    /// <summary>
+    /// Node class.
+    /// </summary>
+    public class Node
+    {
+        /// <summary>
+        /// The value of the Node.
+        /// </summary>
+        public T Value { get; set; }
+
+        /// <summary>
+        /// The next Node in the Queue.
+        /// </summary>
+        public Node Next { get; set; }
+
+        /// <summary>
+        /// Constructor that sets the value of the Node.
+        /// </summary>
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    /// <summary>
+    /// The head Node of the Queue.
+    /// </summary>
+    public Node Head { get; private set; }
+
+    /// <summary>
+    /// The tail Node of the Queue.
+    /// </summary>
+    public Node Tail { get; private set; }
+
+    /// <summary>
+    /// The count of Nodes in the Queue.
+    /// </summary>
+    public int Count { get; private set; }
+
+    /// <summary>
+    /// Constructor that initializes the Queue.
+    /// </summary>
+    public Queue()
+    {
+        Head = null;
+        Tail = null;
+        Count = 0;
+    }
+
+    /// <summary>
+    /// Method that adds a new Node to the Queue.
+    /// </summary>
+    public void Enqueue(T value)
+    {
+        Node node = new Node(value);
+
+        if (Head == null)
+        {
+            Head = node;
+            Tail = node;
+        }
+        else
+        {
+            Tail.Next = node;
+            Tail = node;
+        }
+
+        Count++;
+    }
+
+    /// <summary>
+    /// Method that returns the count of Nodes in the Queue.
+    /// </summary>
+    public int CountNodes()
+    {
+        return Count;
+    }
+}
