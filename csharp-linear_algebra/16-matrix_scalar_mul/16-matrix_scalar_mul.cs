ebddd2d1e2f8390542f5175b3a5b080b099c7597
Caseycjc <knightlaggia@gmail.com>
2023-06-01 17:59:02 -0500

    task16

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,33 @@
+﻿///<summary>
+/// matrixmath class
+///</summary>
+public class MatrixMath
+{
+    /// <summary>
+    /// Multiplies a matrix by a scalar and returns the resulting matrix.
+    /// </summary>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        int rows = matrix.GetLength(0);
+        int columns = matrix.GetLength(1);
+
+        // Check if the matrix is 2D or 3D
+        if ((rows != 2 && rows != 3) || (columns != 2 && columns != 3))
+        {
+            return new double[,] { { -1 } };
+        }
+
+        double[,] result = new double[rows, columns];
+
+        // Multiply each element of the matrix by the scalar
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                result[i, j] = matrix[i, j] * scalar;
+            }
+        }
+
+        return result;
+    }
+}
