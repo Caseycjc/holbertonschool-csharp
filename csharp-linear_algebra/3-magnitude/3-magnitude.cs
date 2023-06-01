fbed1b6224e0806e9a2da480199a88862ebaf091
Caseycjc <knightlaggia@gmail.com>
2023-05-31 23:12:44 -0500

    task3

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class VectorMath
+{
+    /// <summary>
+    /// Calculates the magnitude (length) of a given vector.
+    /// </summary>
+    public static double Magnitude(double[] vector)
+    {
+        int dimensions = vector.Length;
+
+        // Check if the vector is 2D or 3D
+        if (dimensions != 2 && dimensions != 3)
+        {
+            return -1;
+        }
+
+        double sumOfSquares = 0;
+
+        // Calculate the sum of squares of vector components
+        for (int i = 0; i < dimensions; i++)
+        {
+            sumOfSquares += Math.Pow(vector[i], 2);
+        }
+
+        double magnitude = Math.Sqrt(sumOfSquares);
+
+        // Round the magnitude to the nearest hundredth
+        magnitude = Math.Round(magnitude, 2);
+
+        return magnitude;
+    }
+}
