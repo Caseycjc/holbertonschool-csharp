456fb84f3636c79325c3dd463df2047d717eeca9
Caseycjc <knightlaggia@gmail.com>
2023-05-31 22:12:50 -0500

    task3

---
+++ csharp-delegates_events/3-modified_behavior/3-modified_behavior.cs
@@ -0,0 +1,34 @@
+﻿/// <summary>
+/// Represents the strength of a modifier.
+/// </summary>
+public enum Modifier
+{
+    Weak,
+    Base,
+    Strong
+}
+
+/// <summary>
+/// Delegate to calculate modified value.
+/// </summary>
+public delegate float CalculateModifier(float baseValue, Modifier modifier);
+
+public class ModifierUtility
+{
+    /// <summary>
+    /// Applies a modifier to a base value.
+    /// </summary>
+    public static float ApplyModifier(float baseValue, Modifier modifier)
+    {
+        switch (modifier)
+        {
+            case Modifier.Weak:
+                return baseValue / 2;
+            case Modifier.Strong:
+                return baseValue * 1.5f;
+            case Modifier.Base:
+            default:
+                return baseValue;
+        }
+    }
+}
