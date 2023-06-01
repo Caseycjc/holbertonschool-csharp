d7c5557cc7b1f329277b119d88642371ecf6bc9c
Caseycjc <knightlaggia@gmail.com>
2023-06-01 17:46:45 -0500

    task12

---
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,36 @@
+﻿///<summary>
+/// Math Class
+///</summary>
+public class VectorMath
+{
+    /// <summary>
+    /// Calculates the dot product of two vectors.
+    /// </summary>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        int dimensions1 = vector1.Length;
+        int dimensions2 = vector2.Length;
+
+        // Check if the vectors are both 2D or both 3D
+        if ((dimensions1 != 2 && dimensions1 != 3) || (dimensions2 != 2 && dimensions2 != 3))
+        {
+            return -1;
+        }
+
+        // Check if the vectors are of the same size
+        if (dimensions1 != dimensions2)
+        {
+            return -1;
+        }
+
+        double dotProduct = 0;
+
+        // Calculate the dot product by multiplying corresponding components and summing them
+        for (int i = 0; i < dimensions1; i++)
+        {
+            dotProduct += vector1[i] * vector2[i];
+        }
+
+        return dotProduct;
+    }
+}
