0e9bcd0c447d6e3e9b11e03aa1e6b6006cb85d22
Caseycjc <knightlaggia@gmail.com>
2023-05-31 20:34:55 -0500

    task5

---
+++ csharp-interfaces/5-iterate_act/1-user_interface.cs
@@ -0,0 +1,42 @@
+﻿/// <summary>
+/// Interface for interactive objects.
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    /// Method to be implemented for interaction.
+    /// </summary>
+    void Interact();
+}
+
+/// <summary>
+/// Interface for breakable objects.
+/// </summary>
+public interface IBreakable
+{
+    /// <summary>
+    /// Property representing the durability of an object.
+    /// </summary>
+    int durability { get; set; }
+
+    /// <summary>
+    /// Method to be implemented for breaking an object.
+    /// </summary>
+    void Break();
+}
+
+/// <summary>
+/// Interface for collectable objects.
+/// </summary>
+public interface ICollectable
+{
+    /// <summary>
+    /// Property indicating whether the object has been collected.
+    /// </summary>
+    bool isCollected { get; set; }
+
+    /// <summary>
+    /// Method to be implemented for collecting an object.
+    /// </summary>
+    void Collect();
+}
---
+++ csharp-interfaces/5-iterate_act/5-iterate_act.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// The RoomObjects class that contains a method to interact with a list of objects.
+/// </summary>
+public class RoomObjects
+{
+    /// <summary>
+    /// Iterates over a list of objects, interacting with them based on their interfaces.
+    /// </summary>
+    public static void IterateAction(List<Base> roomObjects, Type type)
+    {
+        foreach (var obj in roomObjects)
+        {
+            if (type == typeof(IInteractive) && obj is IInteractive interactive)
+            {
+                interactive.Interact();
+            }
+            else if (type == typeof(IBreakable) && obj is IBreakable breakable)
+            {
+                breakable.Break();
+            }
+            else if (type == typeof(ICollectable) && obj is ICollectable collectable)
+            {
+                collectable.Collect();
+            }
+        }
+    }
+}
