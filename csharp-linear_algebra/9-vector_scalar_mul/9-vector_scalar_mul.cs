d3dffb13fb755154984954253c6f35f1a41a94b3
Caseycjc <knightlaggia@gmail.com>
2023-06-01 17:33:07 -0500

    task9

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,26 @@
+﻿public class VectorMath
+{
+    /// <summary>
+    /// Multiplies a vector by a scalar and returns the resulting vector.
+    /// </summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        int dimensions = vector.Length;
+
+        // Check if the vector is 2D or 3D
+        if (dimensions != 2 && dimensions != 3)
+        {
+            return new double[] { -1 };
+        }
+
+        double[] result = new double[dimensions];
+
+        // Multiply each component of the vector by the scalar
+        for (int i = 0; i < dimensions; i++)
+        {
+            result[i] = vector[i] * scalar;
+        }
+
+        return result;
+    }
+}
