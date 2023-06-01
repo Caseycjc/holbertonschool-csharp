92ba3bf71b201a64baf01823b590af89fe097594
Caseycjc <knightlaggia@gmail.com>
2023-05-31 19:07:14 -0500

    task0

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// The Base abstract class.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Gets or sets the name.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Converts the value of this instance to its equivalent string representation.
+    /// </summary>
+    public override string ToString()
+    {
+        return $"{Name} is a {GetType().Name}";
+    }
+}
