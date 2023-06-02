7dfbee177a5c1f79e66aa037bb1140e996ba4cbb
Caseycjc <knightlaggia@gmail.com>
2023-06-01 20:52:09 -0500

    task26

---
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+///<summary>
+/// math class
+///</summary>
+public class MatrixMath
+{
+    /// <summary>
+    /// Calculates the determinant of a 2D or 3D matrix.
+    /// </summary>
+    public static double Determinant(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if (rows != cols)
+        {
+            return -1;
+        }
+        
+        if (rows == 2)
+        {
+            // Calculate the determinant of a 2D matrix.
+            return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+        }
+        else if (rows == 3)
+        {
+            // Calculate the determinant of a 3D matrix.
+            return matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[1, 2] * matrix[2, 1])
+                 - matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] - matrix[1, 2] * matrix[2, 0])
+                 + matrix[0, 2] * (matrix[1, 0] * matrix[2, 1] - matrix[1, 1] * matrix[2, 0]);
+        }
+        else
+        {
+            // The matrix is not 2D or 3D.
+            return -1;
+        }
+    }
+}
